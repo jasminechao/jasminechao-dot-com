12a05db94d5e6604d4def6dc23cb26b6f1fe27da
Jasmine Chao <ejasminechao@gmail.com>
2018-01-10 10:55:17 +0100

    added js

--- source/javascripts/scrolling.js
+++ source/javascripts/scrolling.js
@@ -1,6 +1,14 @@
-var items = ["Fullstack Developer","Frontend Developer", "Ruby on Rails", "PHP Developer", "Web Design"]
+
+
+var items = ["Fullstack Developer","Frontend Developer", "Ruby on Rails", "PHP Developer", "Web Designer"];
 
 setInterval(function(){
   var item = items[Math.floor(Math.random()*items.length)];
-  $("#scrolling_titles").attr("placeholder", item);
+  $("#scrolling_titles").text(item);
 }, 2000);
+
+// setInterval(function(){
+//   for (var i = 0; i < items.length; i++) {
+//     $("#scrolling_titles").text(items[i]);
+//   }
+// }, 2000);
