b65148263816117247118068f8f9dc8080413051
Jasmine Chao <ejasminechao@gmail.com>
2018-01-11 12:32:22 +0100

    going through scrolling_titles array in order

--- source/javascripts/scrolling.js
+++ source/javascripts/scrolling.js
@@ -1,14 +1,25 @@
+// var titles = ["Fullstack Developer","Frontend Developer", "Ruby on Rails", "PHP Developer", "Web Designer"];
 
-
-var items = ["Fullstack Developer","Frontend Developer", "Ruby on Rails", "PHP Developer", "Web Designer"];
-
-setInterval(function(){
-  var item = items[Math.floor(Math.random()*items.length)];
-  $("#scrolling_titles").text(item);
-}, 2000);
+// setInterval(function(){
+//   var title = titles[Math.floor(Math.random()*titles.length)];
+//   $("#scrolling_titles").text(title);
+// }, 2000);
 
 // setInterval(function(){
 //   for (var i = 0; i < items.length; i++) {
 //     $("#scrolling_titles").text(items[i]);
 //   }
 // }, 2000);
+
+function delayLoop(delay, titles) {
+  var time = 2000;
+
+  $(titles).each(function(k, $this) {
+      setTimeout(function()
+      {
+          $("#scrolling_titles").html($this);
+      }, time)
+      time += delay;
+  });
+}
+delayLoop(2000, ["Fullstack Developer","Frontend Developer", "Ruby on Rails", "PHP Developer", "Web Designer"]);
